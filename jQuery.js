9390dc0997d53fc500f3ae7d6347be6c50f6c045
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2019-12-20 00:28:57 -0700

    updating file

---
+++ jQuery.js
@@ -0,0 +1,22 @@
+// The time
+
+// A field to hold user input
+
+//A save button
+
+//The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).
+
+// Each timeblock contains an input field and save button.
+
+// Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed.
+
+// The current day is displayed at the top of the calendar.
+
+// Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+
+//GIVEN that an employee adds events to a specific hour in a calendar
+
+//WHEN the employee clicks the save button
+
+//THEN events are saved in the timeblock for that hour
+
