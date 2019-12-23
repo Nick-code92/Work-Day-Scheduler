1088b7c7815a422b5f45986aa8fa3a64a0cea47b
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2019-12-23 13:48:07 -0700

    updating file

--- jQuery.js
+++ jQuery.js
@@ -20,21 +20,39 @@
 
 //THEN events are saved in the timeblock for that hour
 
-$(document).ready(function(){
+$(document).ready(function () {
     // console.log("jQurey")
-    function currentDay(){
+    function currentDay() {
         $("#currentDay").text(moment());
 
-     var now = moment().format("HH:mm");
+        var now = moment().format("HH:mm");
 
     }
     setInterval(currentDay, 1000)
-    $(".btn").on("click", function(e){
-       e.preventDefault();
-       var btn = $(this).val();
-       var text = $(btn).val();
-       if (this) {
-       }
-
+    $(".btn").on("click", function (e) {
+        e.preventDefault();
+        var btn = $(this).val();
+        var text = $(btn).val();
     });
-});
+    // var saveBtn = $('.saveBtn');
+    // saveBtn.on('click', function () {
+    //     let eventId = $(this).attr('id');
+    //     let eventText = $(this).parent().siblings().children('.description').val();
+    //     localStorage.setItem(eventId, eventText);
+
+        // function updateColors(){
+        //     var currentTime = new Date().getHours();
+        //     for (var i = 9; i < 18; i++) { 
+        //     console.log(currentTime, $(`#${i}`).data("time"));
+        //      if ($(`#${i}`).data("time") == currentTime){
+        //         $(`#text${i}`).addClass( "present");
+        //     } else if (currentTime < $(`#${i}`).data("time")) {
+        //         $(`#text${i}`).addClass( "future");
+
+        //  }):
+        // function getLocalStorage(key) {
+        //     let value = localStorage.getItem(key);
+        //     if (value) {
+        //         $(`#text${key}`).text(value);
+        //     });
+    });
